550021add3bf042fb55ea5cffb220f8c1c51fcd7
Muhammad Ali Nizami <kustalics@gmail.com>
2024-09-23 10:50:44 +0500

    Task Backend Code Done

--- lib/services/task.dart
+++ lib/services/task.dart
@@ -26,8 +26,40 @@ class TaskServices {
   }
 
   ///Mark Task as Complete
+  Future markTaskAsComplete(TaskModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('task')
+        .doc(model.docId)
+        .update({'isCompleted': true});
+  }
 
   ///Fetch All Task
+  Stream<List<TaskModel>> fetchAllTask() {
+    return FirebaseFirestore.instance.collection('task').snapshots().map(
+        (taskList) => taskList.docs
+            .map((task) => TaskModel.fromJson(task.data()))
+            .toList());
+  }
+
   ///Fetch Completed Task
+  Stream<List<TaskModel>> fetchCompletedTask() {
+    return FirebaseFirestore.instance
+        .collection('task')
+        .where('isCompleted', isEqualTo: true)
+        .snapshots()
+        .map((taskList) => taskList.docs
+            .map((task) => TaskModel.fromJson(task.data()))
+            .toList());
+  }
+
   ///Fetch InCompleted Task
+  Stream<List<TaskModel>> fetchInCompletedTask() {
+    return FirebaseFirestore.instance
+        .collection('task')
+        .where('isCompleted', isEqualTo: false)
+        .snapshots()
+        .map((taskList) => taskList.docs
+        .map((task) => TaskModel.fromJson(task.data()))
+        .toList());
+  }
 }
